361a55cfec1ea36b0ca92e962f11679e0ebcd11b
Ferdous Rahman <117264276+ferdousrah@users.noreply.github.com>
2024-12-09 23:31:35 +0600

    chat is working with django channels and redis server

---
+++ ecommerce_chatbot/asgi.py
@@ -0,0 +1,15 @@
+import os
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import chatbot.routing
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce_chatbot.settings')
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chatbot.routing.websocket_urlpatterns
+        )
+    ),
+})
